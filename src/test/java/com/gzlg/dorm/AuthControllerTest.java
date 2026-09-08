e21d2c9a1d13b9318eee39f3d0613b730f4b1bd9
ilin <786965261@qq.com>
2026-09-08 18:59:05 +0800

    fix(test): 移除 AuthControllerTest 未使用的 Autowired import 消除 IDE 告警

--- src/test/java/com/gzlg/dorm/AuthControllerTest.java
+++ src/test/java/com/gzlg/dorm/AuthControllerTest.java
@@ -3,7 +3,6 @@ package com.gzlg.dorm;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
